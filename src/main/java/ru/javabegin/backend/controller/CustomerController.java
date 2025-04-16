c059f2876a845340f73f3c28c23bf4d75ed3d9a9
MaximKiel <loup.du.nord@yandex.ru>
2025-04-16 20:57:50 +0300

    Create project from lesson 11.61

---
+++ src/main/java/ru/javabegin/backend/controller/CustomerController.java
@@ -0,0 +1,32 @@
+package ru.javabegin.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.javabegin.backend.entity.Customer;
+import ru.javabegin.backend.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+    private CustomerRepository repository;
+
+    // для упрощения кода - сразу будем вызывать метод репозитория, без класса @Service
+    public CustomerController(CustomerRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Customer>> findAll(){
+        List<Customer> list =  repository.findAll();
+        return ResponseEntity.ok(list);
+    }
+
+}
---
+++ src/main/java/ru/javabegin/backend/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package ru.javabegin.backend.repository;
+
+import ru.javabegin.backend.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
